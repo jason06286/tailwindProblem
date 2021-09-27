1e09bbb31fd0df71bd03972ff22a57d4a447f0f2
jason101o <jason101o@gmail.com>
2021-09-27 14:11:22 +0800

    tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./*.{html,js}",
+    "./**/*.{html,js}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
